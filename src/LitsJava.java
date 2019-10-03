e7ad77cc7cdc28c03bfc9cf6aafca47c9e10b1c6
Roman1185 <ramaxin@gmail.com>
2019-10-03 10:16:27 -0700

    commit

--- src/LitsJava.java
+++ src/LitsJava.java
@@ -2,7 +2,8 @@ public class LitsJava {
     public static void main(String[] args) {
       for(int i=0;i<=50;i++){
           char c = (char)i ;
-          System.out.println(c +  i);
+          System.out.println(c +"m" + i);
+
 
 
       /*for(int i=100;i>=0;i--){

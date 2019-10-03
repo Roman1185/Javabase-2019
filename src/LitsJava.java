0940acdc93119f29bb393865c343a75e07504fd0
Roman1185 <ramaxin@gmail.com>
2019-10-03 10:02:49 -0700

    commit

--- src/LitsJava.java
+++ src/LitsJava.java
@@ -1,2 +1,17 @@
-public class Lits {
+public class LitsJava {
+    public static void main(String[] args) {
+      for(int i=0;i<=50;i++){
+          char c = (char)i ;
+          System.out.println(c +  i);
+
+
+      /*for(int i=100;i>=0;i--){
+          System.out.println(i);
+
+       */
+      }
+
+    }
+
 }
+

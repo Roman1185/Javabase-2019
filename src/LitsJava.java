3bfb2284fb1f51f07d78531836f25448bf8a2916
Roman1185 <ramaxin@gmail.com>
2019-10-05 15:28:59 -0700

    commit2

--- src/LitsJava.java
+++ src/LitsJava.java
@@ -1,126 +1,14 @@
 import java.util.Random;
 public class LitsJava {
     public static void main(String[] args) {
-        char c = new Random() {
-
-
-
-
-
-
-
-        /*var a = 0;
-        for (int i =0;i<=4;i++) {
-            if (i % 2 == 0)
-                a = a + i;
-
-        }
-                System.out.println(a);
-
-        var i = new Random().nextInt(100);
-        var k = 1;
-        var s = 0;
-        while (k <= 50) {
-            k++;
-
-            if (i % 7 == 0) {
-                System.out.println(i);
-                s = s + i;
-
-        }
-    }
-}
-           /* i = new Random().nextInt(100);
-        }
-        System.out.print(s);
-        System.out.print("-сума");
-
-            */
-
-
-
-
-
-
-
-       /* var a = new Random().nextInt(1001);
-        System.out.println(a);
-        if(a%3==0) {
-            System.out.println("divers on 3 " );
-        }
-           else if (a%5==0){
-
-                System.out.println("div on 5 ");
-        }
-           else if(a%7==0){
-
-            System.out.println("div on 7 ");
-
-        }
-           else {
-
-            System.out.println("doesnt div");
-        }
-    }
-}
-        /*for (int i = 0; i <= 50; i++) {
-            char c = (char)  i;
-
-            System.out.println(c);
-
-        }
-    }
-}
-        /*var k = 0;
-        for (int i = 0; i <= 15; i++) {
+        var a = 0;
+        for(int i =1;i<=15;i++) {
             if (i % 2 == 0) {
-                k =k+i;
+                a = a + i;
             }
         }
-
-        System.out.println(k);
-
-         */
-         /* var sum=0;
-          for(int i=0;i<=10;i++) {
-              if (i %1  == 0) {
-                  sum = sum + i;
-              }
-          }
-
-          System.out.println(sum);
-
-         /*for(int i=1;i<=100;i++){
-             System.out.println(i);
-       /* var i = 1;
-        while (i <= 10) {
-            Random rnd = new Random();
-            char c = (char) (rnd.nextInt(26) + 'a');
-            i++;
-            System.out.print(c);
-
-        */
-/*var rand=new Random().nextInt(11);
-int b=0;
-for(int i =0;i<=100)
-
-
-    System.out.println("Number is " +  rand);
-
-var a=0;
-for(int i=1;i<=15;i++) {
-    if (i % 2 == 0) {
-        a = a + i;
+            System.out.println(a);
+        }
     }
-}
-    System.out.println(a);
-}
-
- */
-//jghvkfxghviduyghvvncbf djkvbn gjvbh
-
-
-
-
 
 
